680dbc9e76c4f0f7826dedf010e0cef2e27b6318
Fred <fpaula@gmail.com>
2013-10-18 13:33:07 -0300

    Watch position

--- assets/javascripts/geo.js
+++ assets/javascripts/geo.js
@@ -1,48 +1,40 @@
 var Geo = {
   _userGeo: null,
+  _watchId: null,
   get: function(successCallback) {
     if (this._userGeo) {
       return successCallback(this._userGeo);
     }
-    var options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    };
     navigator.geolocation.getCurrentPosition(
       function(position){
         Geo._userGeo = position;
         successCallback(position);
       },
-      function(error){
-        console.warn('ERROR(' + error.code + '): ' + error.message);
-      },
-      options
+      this.error, this.options
     );
   },
-  print: function(containerSelector) {
-    this.get(function(geo){
-      var container = $(containerSelector);
-      var ul = $('<ul></ul>');
-      ul.append(Geo._createPrintElement("Speed", geo.speed));
-      ul.append(Geo._createPrintElement("Heading", geo.heading));
-      ul.append(Geo._createPrintElement("Altitude Accuracy", geo.altitudeAccuracy));
-      ul.append(Geo._createPrintElement("Accuracy", geo.accuracy));
-      ul.append(Geo._createPrintElement("Altitude", geo.altitude));
-      ul.append(Geo._createPrintElement("Longitude", geo.longitude));
-      ul.append(Geo._createPrintElement("Latitude", geo.latitude));
-      container.append(ul);
-
-      return container;
-    });
+  watch: function(successCallback) {
+    this._watchId = navigator.geolocation.watchPosition(
+      function(position) {
+        successCallback(position);
+    },
+    this.error, this.options);
+  },
+  clearWatch: function() {
+    navigator.geolocation.clearWatch(this._watchId);
+    this._watchId = null;
+    return true;
   },
   _createPrintElement: function (key, value) {
     return $("<li>" + key + ": " + value + "</li>");
+  },
+  _error: function(error) {
+    console.warn('ERROR(' + error.code + '): ' + error.message);
+  },
+  _options: function() {
+    //TODO: options as params
+    return { enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0 }
   }
 };
-
-Zepto(function($){
-  $('button').on('click', function(){
-    Geo.print('.result');
-  });
-});
